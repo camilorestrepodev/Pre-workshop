a5f28bc8f9ff148868d8f8cdd72e9974ed2c6830
Camilo Restrepo <camilorestrepob5@gmail.com>
2023-02-06 18:12:02 -0500

    v2

--- src/Hospital.java
+++ src/Hospital.java
@@ -1,21 +1,22 @@
 import java.time.LocalDate;
 import java.util.Date;
+import java.util.Objects;
 
 public class Hospital {
     private int valorBase;
 
     public void agendarCita(String especialidad, String usuario, Date fechaCita){
-        if(usuario == "EPS"){
+        if(Objects.equals(usuario, "EPS")){
             this.valorBase = 30000;
             System.out.println("El tipo de usuario es: " + usuario + " ,la especialidad es: " + especialidad + " ,la fecha de la cita es: " + fechaCita + " ,el precio de la cita es: " + valorBase);
         }
 
-        if(usuario == "POLIZA"){
+        if(Objects.equals(usuario, "POLIZA")){
             this.valorBase = 40000;
             System.out.println("El tipo de usuario es: " + usuario + " ,la especialidad es: " + especialidad + " ,la fecha de la cita es: " + fechaCita + " ,el precio de la cita es: " + valorBase);
         }
 
-        if(usuario == "PARTICULAR"){
+        if(Objects.equals(usuario, "PARTICULAR")){
             this.valorBase = 70000;
             System.out.println("El tipo de usuario es: " + usuario + " ,la especialidad es: " + especialidad + " ,la fecha de la cita es: " + fechaCita + " ,el precio de la cita es: " + valorBase);
         }
